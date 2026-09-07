30170bc62a446319ccf796c39673d62eeaf0c7bc
Nandini <nandiniagarwal291@gmail.com>
2026-09-07 20:34:34 +0530

    Fix PORT initialization for Render

--- app.js
+++ app.js
@@ -24,10 +24,12 @@ app.use("/", historyRoutes);
 app.use("/", analyticsRoutes);
 connectDB();
 
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
